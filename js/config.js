5900d00cb5dc90d7c0dd7d9d80c1c890f7d9494e
Genesyxx <95386889+Genesyxx@users.noreply.github.com>
2025-03-03 12:22:32 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_1XQu5hRwC', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1v1tqc6n92qb6f9ti63vnpsmag', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: '' // e.g. us-east-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
